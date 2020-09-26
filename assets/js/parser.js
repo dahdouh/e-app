fd23058ca3bff37ac3d242d8925ab4008700c434
dahdouh <karim.dahdouh.fr@gmail.com>
2020-09-27 01:20:24 +0200

    entries

--- assets/js/parser.js
+++ assets/js/parser.js
@@ -11,6 +11,8 @@ $(document).ready(function(){
 
 function getWordDescription() {
     document.getElementById("ba").innerHTML = "";
+    document.getElementById("definition").innerHTML = "";
+    
     document.getElementById("spinner").style.display = "";
     var word = document.getElementById("searchedWord").value;
     
@@ -22,10 +24,38 @@ function getWordDescription() {
         success: function(data) {
             document.getElementById("spinner").style.display = "none";
             document.getElementById("searchedWord").value = "";
-            $("#ba").html(data)
+            //$("#ba").html(data);
+            //var htmlTagRe = /<\/?[\w\s="/.':;#-\/\?]+>/gi;
+            //var plainText = html.replace(htmlTagRe, '');
+            //var str = "The rain in SPAIN stays mainly in the plain";
+            //var res = data.match(//<\s*def[^>]*>(.*?)<\s*/\s*def>/g);
+            //var res = data.match(/(e;[0-9]+;\'[\w\'áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ.+>=&;:?! -]+\';[0-9]+;[0-9]+.*)|(r;[0-9]+;[0-9]+;[0-9]+;[0-9]+;[0-9]+)/m);
+            
+            //var str = "<def><br />  Université. (forme utilisée surtout en Belgique)</def>";
+            
+
+            var str = data.replace(/(\r\n|\n|\r)/gm," ");
+            var definitions = str.match(/<def>(.*?)<\/def>/g).map(function(val){
+                                        return val.replace(/<\/?def>/g,'');
+                                        });
+            definitions.forEach(item => console.log(item));
+            
+            definitions.forEach(item => {
+                var data = '<div class="alert alert-primary" role="alert"> '+item+'</div>';
+                document.getElementById('definition').innerHTML += data;
+
+            });
+
+            str = data.replace(/(\r\n|\n|\r)/gm," ");
+            var entries = str.match(/(e;[0-9]+;\'[\w\'áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ.+>=&;:?! -]+\';[0-9]+;[0-9]+.*)|(r;[0-9]+;[0-9]+;[0-9]+;[0-9]+;[0-9]+)/g).map(function(val){
+                                        return val.replace(/<\/?def>/g,'');
+                                        });
+            $("#ba").html(entries);
+
+
         },
         error: function(data) {
-            alert("error : " + data);
+            alert("error : " + data[0]);
         }
     });
 }
