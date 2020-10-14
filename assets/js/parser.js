d5362d60e6a6a5a87a01fd048361c96dfcac80f0
dahdouh <karim.dahdouh.fr@gmail.com>
2020-10-15 00:16:12 +0200

    ouverture

--- assets/js/parser.js
+++ assets/js/parser.js
@@ -9,113 +9,137 @@ $(document).ready(function(){
 })
 
 
-function getWordDescription() {
+function getWordDescription(search) {
+        document.getElementById("ba").innerHTML = "";
+        document.getElementById("definition").innerHTML = "";
+        
+        document.getElementById("spinner").style.display = "";
+
+        let word ="" ;
+        if(typeof search !== 'undefined' || document.getElementById("searchedWord").value == ''){  
+            word = search;
+        } else {
+            word = document.getElementById("searchedWord").value;
+        }
+
+        
+        $.ajax({
+            url: 'http://localhost/e-app/server/index.php',
+            type: 'GET',
+            data: 'word=' + word,
+
+            success: function(data) {
+                document.getElementById("spinner").style.display = "none";
+                document.getElementById("searchedWord").value = "";
+                //$("#ba").html(data);
+                //console.log(data);
+                //var htmlTagRe = /<\/?[\w\s="/.':;#-\/\?]+>/gi;
+                //var plainText = html.replace(htmlTagRe, '');
+                //var str = "The rain in SPAIN stays mainly in the plain";
+                //var res = data.match(//<\s*def[^>]*>(.*?)<\s*/\s*def>/g);
+                //var res = data.match(/(e;[0-9]+;\'[\w\'áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ.+>=&;:?! -]+\';[0-9]+;[0-9]+.*)|(r;[0-9]+;[0-9]+;[0-9]+;[0-9]+;[0-9]+)/m);
+                
+                //var str = "<def><br />  Université. (forme utilisée surtout en Belgique)</def>";
+                
+
+                var str = data.replace(/(\r\n|\n|\r)/gm," ");
+                var definitions = str.match(/<def>(.*?)<\/def>/g).map(function(val){
+                                            return val.replace(/<\/?def>/g,'');
+                                            });
+                //definitions.forEach(item => console.log(item));
+                
+                definitions.forEach(item => {
+                    var data = '<div class="alert alert-primary" role="alert"> '+item+'</div>';
+                    document.getElementById('definition').innerHTML += data;
+
+                });
+
+                str = data.replace(/(\r\n|\n|\r)/gm," ");
+
+                //Les entries
+                var entries = str.match(/(e;[0-9]+;\'[\w\'áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ.+>=&;:?! -]+\';[0-9]+;[0-9]+.*)|(r;[0-9]+;[0-9]+;[0-9]+;[0-9]+;[0-9]+)/g).map(function(val){
+                    return val.replace(/<\/?def>/g,'');
+                    });
+                var all_entries=entries.toString().split("<br>");
+                var IdFirstWord = all_entries[0].split(";")[1];
+                //console.log(IdFirstWord)
+                
+                var filtred_entries = [];
+                //relations sortantes
+                var rel_sortantes = str.match(new RegExp('(r;[0-9]+;[0-9]+;'+IdFirstWord+';[0-9]+;[0-9]+)','g')).map(function(val){
+                    return val.replace(/<\/?def>/g,'');
+                    });
+                rel_sortantes.forEach(item => {
+                    var one_entry=item.split(";");
+                    filtred_entries.push(one_entry[2]); 
+                });
+
+                //relations entrantes
+                var rel_entrantes = str.match(new RegExp('(r;[0-9]+;'+IdFirstWord+';[0-9]+;[0-9]+;[0-9]+)','g')).map(function(val){
+                    return val.replace(/<\/?def>/g,'');
+                    });
+                rel_entrantes.forEach(item => {
+                    var one_entry=item.split(";");
+                    filtred_entries.push(one_entry[3]); 
+                });
+
+    
+        
+                /**
+                 * Trier le résultat de recherche
+                 */
+                //Mots de relations entrantes;
+                var mots_sorted = [];
+                filtred_entries.slice(1, 40).forEach(item => {
+
+                    //entrées
+                    var correspond_entries = str.match(new RegExp("(e;"+item+";.*;[0-9]+;[0-9]+)",'g')).map(function(val){
+                        return val.replace(/<\/?def>/g,'');
+                    });
+
+                    correspond_entries.forEach(item => {
+                        var word_entry=item.split(";");
+                        mots_sorted.push(word_entry[2]); 
+                    });
+                });
+
+                // show sorted results
+                var htlm_entrantes ="";
+                    mots_sorted.sort((a, b) => a.localeCompare(b, undefined, {sensitivity: 'base'})).forEach(item => { 
+                        //delete quote
+                        let word = item.substring(1, item.length - 1);
+                        //htlm_entrantes = '<span class="badge badge-warning" title="thems hoverin words"> <a style="color:purple;opacity:0.8;" onclick="getEntryCorrespond('+word+')">'+ word +' </span> &nbsp;&nbsp;';
+                        htlm_entrantes = '<span class="badge badge-warning" title="thems hoverin words"> <a style="color:purple;opacity:0.8;" href="http://localhost/e-app/?word='+word+'">'+ word +' </span> &nbsp;&nbsp;';
+                        document.getElementById('ba').innerHTML += htlm_entrantes;
+                        
+                    });
+
+
+
+
+
+                
+                                        
+                //$("#ba").html(entries);
+
+
+            },
+            error: function(data) {
+                alert("error : " + data[0]);
+            }
+        });
+    
+}
+
+function getEntryCorrespond(word) {
+   getWordDescription(word);
+   /*
     document.getElementById("ba").innerHTML = "";
     document.getElementById("definition").innerHTML = "";
     
     document.getElementById("spinner").style.display = "";
     var word = document.getElementById("searchedWord").value;
-    
-    $.ajax({
-        url: 'http://localhost/e-app/server/index.php',
-        type: 'GET',
-        data: 'word=' + word,
-
-        success: function(data) {
-            document.getElementById("spinner").style.display = "none";
-            document.getElementById("searchedWord").value = "";
-            //$("#ba").html(data);
-            //console.log(data);
-            //var htmlTagRe = /<\/?[\w\s="/.':;#-\/\?]+>/gi;
-            //var plainText = html.replace(htmlTagRe, '');
-            //var str = "The rain in SPAIN stays mainly in the plain";
-            //var res = data.match(//<\s*def[^>]*>(.*?)<\s*/\s*def>/g);
-            //var res = data.match(/(e;[0-9]+;\'[\w\'áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ.+>=&;:?! -]+\';[0-9]+;[0-9]+.*)|(r;[0-9]+;[0-9]+;[0-9]+;[0-9]+;[0-9]+)/m);
-            
-            //var str = "<def><br />  Université. (forme utilisée surtout en Belgique)</def>";
-            
-
-            var str = data.replace(/(\r\n|\n|\r)/gm," ");
-            var definitions = str.match(/<def>(.*?)<\/def>/g).map(function(val){
-                                        return val.replace(/<\/?def>/g,'');
-                                        });
-            //definitions.forEach(item => console.log(item));
-            
-            definitions.forEach(item => {
-                var data = '<div class="alert alert-primary" role="alert"> '+item+'</div>';
-                document.getElementById('definition').innerHTML += data;
-
-            });
-
-            str = data.replace(/(\r\n|\n|\r)/gm," ");
-
-            //Les entries
-            var entries = str.match(/(e;[0-9]+;\'[\w\'áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ.+>=&;:?! -]+\';[0-9]+;[0-9]+.*)|(r;[0-9]+;[0-9]+;[0-9]+;[0-9]+;[0-9]+)/g).map(function(val){
-                return val.replace(/<\/?def>/g,'');
-                });
-            var all_entries=entries.toString().split("<br>");
-            var IdFirstWord = all_entries[0].split(";")[1];
-            //console.log(IdFirstWord)
-            
-
-            //recupérer es id des relations entrantes
-            var rel_entrantes = str.match(new RegExp("(r;[0-9]+;[0-9]+;"+IdFirstWord+";[0-9]+;[0-9]+)",'g')).map(function(val){
-                return val.replace(/<\/?def>/g,'');
-                });
-                //console.log(rel_entrantes);
-            //$("#ba").html(rel_entrantes);
-
-            // IDs relations entrantes
-            var filtred_entries = [];
-            rel_entrantes.forEach(item => {
-                var one_entry=item.split(";");
-                filtred_entries.push(one_entry[2]); 
-            });
-
-    
-            /**
-             * Trier le résultat de recherche
-             */
-            //Mots de relations entrantes;
-            filtred_entries.slice(1, 40).forEach(item => {
-                var correspond_entries = str.match(new RegExp("(e;"+item+";.*;[0-9]+;[0-9]+)",'g')).map(function(val){
-                    return val.replace(/<\/?def>/g,'');
-                });
-
-                var mots_sorted = [];
-                correspond_entries.forEach(item => {
-                    var word_entry=item.split(";");
-                    mots_sorted.push(word_entry[2]); 
-                });
-                var htlm_entrantes ="";
-                mots_sorted.forEach(item => { 
-                    htlm_entrantes = '<span class="badge badge-warning"> <a style="color:purple;opacity:0.8;" href="http://www.jeuxdemots.org/diko.php?gotermrel='+ item +'">'+ item +' </span> &nbsp;&nbsp;';
-                    document.getElementById('ba').innerHTML += htlm_entrantes;
-                    
-                });
-                //document.getElementById('ba').innerHTML = mots_sorted;
-                
-
-                //console.log(correspond_entries);
-
-                //var data = '<span class="badge badge-warning"> <a style="color:purple;opacity:0.8;" href="http://www.jeuxdemots.org/diko.php?gotermrel='+ item +'">'+ item +' </span> &nbsp;&nbsp;';
-                //document.getElementById('ba').innerHTML += data;
-            });
-
-
-
-
-
-            
-                                      
-            //$("#ba").html(entries);
-
-
-        },
-        error: function(data) {
-            alert("error : " + data[0]);
-        }
-    });
+    */
 }
 
 function writeDataInFile(data) {
--- index.php
+++ index.php
@@ -9,9 +9,10 @@
         <!-- <link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen"> -->
         <!-- My style -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-
-
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+        <script src="http://code.jquery.com/jquery.js"></script>
+        <script src="assets/js/parser.js"></script>
 
     </head>
     <body>
@@ -75,7 +76,7 @@
             <div class="card-header" id="headingTwo">
               <h5 class="mb-0">
                 <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                  Relations entrantes
+                  Relations
                 </button>
               </h5>
             </div>
@@ -85,11 +86,28 @@
               </div>
             </div>
           </div>
+
+          <?php
+    $word = "";
+    if(isset($_GET['word'])){
+        $word = $_GET["word"];
+      ?>
+    
+      <script type="text/javascript">
+        var word = '<?php echo $word; ?>';
+        console.log(word);
+        getWordDescription(word);
+       </script>
+
+      <?php
+    }
+    ?>
+          <!--
           <div class="card">
             <div class="card-header" id="headingThree">
               <h5 class="mb-0">
                 <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                  Relations sortantes
+                  Relations
                 </button>
               </h5>
             </div>
@@ -99,6 +117,7 @@
               </div>
             </div>
           </div>
+          -->
         </div>
 <!--
         <div id="result" class="card">
@@ -119,9 +138,9 @@
 
      
           <footer>
-              <script src="http://code.jquery.com/jquery.js"></script>
+              
               <!-- <script src="assets/js/bootstrap.min.js"></script> -->
-              <script src="assets/js/parser.js"></script>
+              
               <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
               <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
